6e50f546bfb2a102b0fb241b71ea45b6f61ae26f
Alice Won <alicewon4@gmail.com>
2019-10-23 20:21:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
 
   length.times do |index|
     if array[index] = value_to_find then
-      return array[index]
+      return array.index(value_to_find)
   end
 end
 
