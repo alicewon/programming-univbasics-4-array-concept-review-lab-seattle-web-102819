e454ebe9332f625c2e14be222d6f7cc33b921b87
Alice Won <alicewon4@gmail.com>
2019-10-23 21:27:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,14 @@
 def find_element_index(array, value_to_find)
+  length = array.length
 
+  length.times do 
+    return array.index("value_to_find")
+  end
 end
 
 
 def find_max_value(array)
   return array.sort[-1]
-
 end
 
 def find_min_value(array)
