c2a8d2dae6f7c9afe3dbaea9c62829cda2a2c219
Alice Won <alicewon4@gmail.com>
2019-10-23 20:47:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,6 @@
 def find_element_index(array, value_to_find)
-  length = array.length
-  length.times do |index|
-    if array[index] = value_to_find then
-      puts index
-    end
-  end
+  if array.include?(value_to_find) then
+    return array,index
 end
 
 
